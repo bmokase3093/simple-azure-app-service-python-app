c63e4e624ee0c7de8c34e617fdb663a230db0b12
Bongani Mokase <bonganimokase@outlook.com>
2026-04-24 00:39:19 +0200

    test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Azure App Service!"
+    return "Hello from Azure App Service!Test"
 
 if __name__ == "__main__":
     app.run()
